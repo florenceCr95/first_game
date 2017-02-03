4099607e2cd456d3b892e8cab819c089c1a5a6b9
florenceCr95 <flo.cretenoud@gmail.com>
2017-02-03 16:44:47 +0000

    prevent character from going out of the page

--- client/main.js
+++ client/main.js
@@ -7,23 +7,25 @@ Template.Character.onCreated(() => {
     $(document).on('keypress', function (event) {
         // Using WASD because my arrows aren't detected
         if(event.keyCode == 97) { // left
-            console.log("left");
             var inc = parseInt($("#character").css("left").slice(0, -2)) - 10;
+            inc = inc < 0 ? 0 : inc;
             $("#character").css("left", inc);
         }
         if(event.keyCode == 119) { // up
-            console.log("up");
             var inc = parseInt($("#character").css("top").slice(0, -2)) - 10;
+            inc = inc < 0 ? 0 : inc;
             $("#character").css("top", inc);
         }
         if(event.keyCode == 100) { // right
-            console.log("right");
             var inc = parseInt($("#character").css("left").slice(0, -2)) + 10;
+            var max = $(window).width() - 30;
+            inc = inc > max ? max : inc;
             $("#character").css("left", inc);
         }
         if(event.keyCode == 115) { // down
-            console.log("down");
             var inc = parseInt($("#character").css("top").slice(0, -2)) + 10;
+            var max = $(window).height() - 30;
+            inc = inc > max ? max : inc;
             $("#character").css("top", inc);
         }
     });
